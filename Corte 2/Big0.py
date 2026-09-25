025662c72b846a900ebae85aba44ae5a9b2826b7
AndresOrtegon23 <ortegonporrasandresfelipe@gmail.com>
2026-09-25 08:16:48 -0500

    fix: Se corrigio la complejidad del algoritmo

--- Corte 2/Big0.py
+++ Corte 2/Big0.py
@@ -21,7 +21,7 @@ def insertionSort(arr):
     return arr, comparacion
 
 #Secuuencia de pasos de un algoritmo secuencial de ordenamiento por selección
-#1. ¿Que es n en esa secuencia? = n es el total de elementos en la lista que se va a ordenar.
+#1. ¿Que es n en esa secuencia hablando de complejidad? = n es el total de elementos en la lista que se va a ordenar (O(n^2))
 #2. ¿Cuantas veces se ejecuta el proceso si se duplican los datos? = Si se duplican los datos, el proceso se ejecuta el doble de veces, ya que n se incrementa y el bucle for depende de n.
 #3. Dentro de esa secuencia se invoca otro proceso, ¿cuantos procesos se invocan? = En esta secuencia se invoca un proceso de búsqueda del mínimo en cada iteración del bucle externo, por lo que se invoca n veces.
 #4. ¿Que tipo de estructura de datos se utliza? = En esta estructura de datos se utiliza una lista (array) para almacenar los elementos que se van a ordenar.
